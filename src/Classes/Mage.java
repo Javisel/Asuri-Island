ebec053a8bad9c0f3bc3d9035eaa10e9fbb5d681
Javisel <36491374+Javisel@users.noreply.github.com>
2018-04-15 15:00:00 -0400

    Update Mage.java

--- src/Classes/Mage.java
+++ src/Classes/Mage.java
@@ -6,11 +6,11 @@ public class Mage extends Entity{
 
 	public Mage(String name) {
 		
-		super();
+		super("Roxane", "Mage");
 		this.setEntityclass("Mage");
 		this.setName(name);
 		this.fillstats();
-		
+		this.getClass().asSubclass(Entity.class);
 }
 
 	
